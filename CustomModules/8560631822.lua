365c18fb9e5b6cff1968c88b359d8672569fc41e
gscoutis <97824421+gscoutis@users.noreply.github.com>
2023-02-10 18:29:19 -0800

    Update 8560631822.lua

--- CustomModules/8560631822.lua
+++ CustomModules/8560631822.lua
@@ -3,7 +3,7 @@ local checkpublicreponum = 0
 local checkpublicrepo
 checkpublicrepo = function(id)
 	local suc, req = pcall(function() return requestfunc({
-		Url = "https://raw.githubusercontent.com/gscoutis/VapeV4ForRoblox/main/CustomModules/"..id..".lua",
+		Url = "https://raw.githubusercontent.com/gscoutis/VapeV4ForRoblox/main/"..readfile("vape/commithash.txt").."/CustomModules/"..id..".lua",
 		Method = "GET"
 	}) end)
 	if not suc then
